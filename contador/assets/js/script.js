54f9ee35e7fc061603e04df679a74c82b7cc7175
claudiostreppel <claudio.ricardo.streppel@gmail.com>
2021-12-22 18:34:03 -0300

    adicionado msg de soma/subtração no contador

--- contador/assets/js/script.js
+++ contador/assets/js/script.js
@@ -1,13 +1,18 @@
 let currentNumberWrapper = document.getElementById("currentNumber")
 let currentNumber = 0
+let msgtxt = document.getElementById("msg")
+let msgsoma = 'você somou 1'
+let msgsubt = 'voce subtraiu 1'
 
 function increment() {
     console.log(currentNumberWrapper.innerHTML)
     currentNumber += 1
     currentNumberWrapper.innerHTML = currentNumber
+    msgtxt.innerHTML=msgsoma
 }
 function decrement() {
     console.log(currentNumberWrapper.innerHTML)
     currentNumber -= 1
     currentNumberWrapper.innerHTML = currentNumber
+    msgtxt.innerHTML = msgsubt
 }
