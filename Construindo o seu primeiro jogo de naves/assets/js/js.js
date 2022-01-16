e4c89599d80cd272413486b7489a3c2ec4ca2776
claudiostreppel <claudio.ricardo.streppel@gmail.com>
2022-01-16 08:30:03 -0300

    inimigo1 move para a esquerda

--- Construindo o seu primeiro jogo de naves/assets/js/js.js
+++ Construindo o seu primeiro jogo de naves/assets/js/js.js
@@ -8,6 +8,8 @@ function start() {
     //Principais variáveis do jogo
 	
     var jogo = {}
+    var velocidade=5;
+    var posicaoY = parseInt(Math.random() * 334);
     var TECLA = { W: 87, S: 83, D: 68 }
 	
     jogo.pressionou = [];
@@ -31,6 +33,7 @@ function start() {
 	
         movefundo();
         movejogador();
+        moveinimigo1();
 
 	
     } // Fim da função loop()
@@ -73,5 +76,19 @@ function start() {
         }
 
     } // fim da função movejogador()
+
+    function moveinimigo1() {
+
+	posicaoX = parseInt($("#inimigo1").css("left"));
+	$("#inimigo1").css("left",posicaoX-velocidade);
+	$("#inimigo1").css("top",posicaoY);
+		
+		if (posicaoX<=0) {
+		posicaoY = parseInt(Math.random() * 334);
+		$("#inimigo1").css("left",694);
+		$("#inimigo1").css("top",posicaoY);
+			
+		}
+} //Fim da função moveinimigo1()
 
 }
